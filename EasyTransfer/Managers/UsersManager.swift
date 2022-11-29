b46e42de7dee69be909388b693d366b4a1fe9a9e
Nikita Aleksejevas <nik.aleksejev@gmail.com>
2022-11-29 11:23:32 +0200

    codded func Register in UserManager class

--- EasyTransfer/Managers/UsersManager.swift
+++ EasyTransfer/Managers/UsersManager.swift
@@ -9,5 +9,22 @@ import Foundation
 
 class UsersManager {
     
+    var users: [User] = []
+    
+    func register(username: String, password: String, confirmpassword: String){
+        
+        guard !username.isEmpty, !password.isEmpty else {
+            return
+        }
+        
+        guard users.contains(where: { user in username != user.username }) else {
+            return
+        }
+        guard password == confirmpassword else {
+            return
+        }
+    
+        
+    }
     
 }
